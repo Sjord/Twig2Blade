e41a498204e0ef15ab9424456de1dae601fb95db
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2024-04-09 13:18:48 +0200

    GreaterEqualBinary

--- src/Sjord/Twig2Blade/Node/Expression/Binary/GreaterEqualBinary.php
+++ src/Sjord/Twig2Blade/Node/Expression/Binary/GreaterEqualBinary.php
@@ -1,3 +1,5 @@
 <?php
 namespace Sjord\Twig2Blade\Node\Expression\Binary;
-final class GreaterEqualBinary extends AbstractBinary { }
+final class GreaterEqualBinary extends AbstractBinary { 
+    protected $operator = '>=';
+}
