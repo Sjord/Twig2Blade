459fc90aad3cf17cacede963915fc42349ef0adc
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2024-04-17 21:06:31 +0200

    Support @elseif

--- src/Sjord/Twig2Blade/Node/IfNode.php
+++ src/Sjord/Twig2Blade/Node/IfNode.php
@@ -4,11 +4,14 @@ use \Twig\Compiler;
 class IfNode extends \Sjord\Twig2Blade\Node\Node {
     public function compile(Compiler $compiler): void
     {
+        $directive = '@if';
         foreach (array_chunk($this->getNode('tests')->nodes, 2) as [$test, $block]) {
-            $compiler->raw('@if (')
+            $compiler->raw($directive)
+                ->raw(' (')
                 ->subcompile($test)
                 ->raw(")\n")
                 ->subcompile($block);
+            $directive = '@elseif';
         }
 
         if ($this->hasNode('else')) {
