6833094e620be567a2d3dba98e1baacf83013218
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2024-04-17 15:58:12 +0200

    Support do tag

--- src/Sjord/Twig2Blade/Node/DoNode.php
+++ src/Sjord/Twig2Blade/Node/DoNode.php
@@ -1,5 +1,12 @@
 <?php
 namespace Sjord\Twig2Blade\Node;
+use \Twig\Compiler;
 class DoNode extends \Sjord\Twig2Blade\Node\Node {
-
+    public function compile(Compiler $compiler): void
+    {
+        $compiler
+            ->raw("@php\n")
+            ->subcompile($this->getNode('expr'))
+            ->raw(";\n@endphp\n");
+    }
 }
